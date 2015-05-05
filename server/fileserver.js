3a75cfdfa531d3562394e6067675e07dac2f895e
Sam Delesque <sdelesque@domanistudios.com>
2015-05-05 12:19:56 -0400

    index fallback

--- server/fileserver.js
+++ server/fileserver.js
@@ -4,7 +4,11 @@ var port = 2012
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        file.serve(request, response);
+        file.serve(request, response, function (e, res) {
+            if (e && (e.status === 404)) { // If the file wasn't found
+                file.serveFile('/index.html', 404, {}, request, response);
+            }
+        });
     }).resume();
 }).listen(port);
 
